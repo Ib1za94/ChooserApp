dbb2781303e54e0c19bf8e0bbd3e33e6de6a6b7d
Ib1za94 <kolibelrazuma@gmail.com>
2024-02-01 04:55:17 +0200

    Added the ability to return to the main menu from the settings screen

--- app/src/main/java/com/example/chooserapp/SettingsScreenFragment.kt
+++ app/src/main/java/com/example/chooserapp/SettingsScreenFragment.kt
@@ -7,25 +7,30 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.ImageView
 
 class SettingsScreenFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //TODO: Разобраться как повесить онкликлистенер в фрагменте и свернуть его
-//        val exitButton: ImageButton = view.findViewById(R.id.crossImageView)
-//        exitButton.setOnClickListener{
-//            supportFragmentManager.beginTransaction()
-//                .remove(SettingsScreenFragment())
-//                .commit()
-//        }
-            // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.setting_screen, container, false)
+        val view = inflater.inflate(R.layout.setting_screen, container, false)
 
+        val closeButton: ImageView? = view?.findViewById(R.id.settings_cross)
+        closeButton?.setOnClickListener {
+            // Закрываем текущий фрагмент
+            requireActivity().supportFragmentManager.beginTransaction()
+                .remove(this@SettingsScreenFragment)
+                .commit()
 
+            // Возвращаемся на главный экран (MainActivity)
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+
+        return view
     }
 
+//  Сделал хуиту, дальше ебану кнопочки на music и sound, ну и попробую поставить код на смену языка
     companion object {
 
         @JvmStatic
